9b5b5d23f978bfa2e1efdbfbbac8bbb0e2985ad6
유관우 <skymill2000@gmail.com>
2023-02-21 13:06:21 +0900

    [feat] 구조분해 할당 예제

---
+++ prac/example05.js
@@ -0,0 +1,13 @@
+let car = {
+  name: "sonata",
+  ph: 160,
+  year: 2022,
+};
+
+const { name, ph, year } = car;
+
+// let name = car.name;
+// let ph = car.ph;
+// let year = car.year;
+
+console.log(name, ph, year);
