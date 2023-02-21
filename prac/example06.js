a10f597fa37b81856065d147800d9d6d9d03427c
유관우 <skymill2000@gmail.com>
2023-02-21 13:12:14 +0900

    [feat] 템플릿 리터럴 문법 실습

---
+++ prac/example06.js
@@ -0,0 +1,10 @@
+let car = {
+  name: "sonata",
+  ph: 160,
+  year: 2022,
+};
+
+const { name, ph, year } = car;
+
+console.log(name + "는 " + ph + " 마력 " + year + " 년식 입니다.");
+console.log(`${name}는 ${ph} 마력 ${year} 년식 입니다.`);
