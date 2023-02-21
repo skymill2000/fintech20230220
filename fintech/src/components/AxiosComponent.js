f6bc5e731259ce37d8393a0c41ae81f756e9820f
유관우 <skymill2000@gmail.com>
2023-02-21 16:53:27 +0900

    [feat] axios 실습 cors 문제 해결 1  - cors 크롬 익스텐션을 활용한 해결  - youtube.com get 요청 데이터 확인

--- fintech/src/components/AxiosComponent.js
+++ fintech/src/components/AxiosComponent.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const AxiosComponent = () => {
+  const [data, setData] = useState();
+
   const handleClick = () => {
     console.log("서버로 요청을 보냅니다.");
-    const requestUrl = "https://naver.com";
+    const requestUrl = "https://www.youtube.com";
     axios.get(requestUrl).then((response) => {
       console.log(response);
+      setData(response.data);
     });
   };
   return (
     <div>
       <button onClick={handleClick}>데이터 통신하기</button>
+      <p>{data}</p>
     </div>
   );
 };
