25e1d6460b788c5d93be04abaf195fa258b5d816
유관우 <skymill2000@gmail.com>
2023-02-21 16:40:07 +0900

    [feat] axios 모듈 추가  - npm install axios  - axios 모듈 사용 예제 작성

--- fintech/src/components/AxiosComponent.js
+++ fintech/src/components/AxiosComponent.js
@@ -1,8 +1,13 @@
 import React from "react";
+import axios from "axios";
 
 const AxiosComponent = () => {
   const handleClick = () => {
     console.log("서버로 요청을 보냅니다.");
+    const requestUrl = "https://naver.com";
+    axios.get(requestUrl).then((response) => {
+      console.log(response);
+    });
   };
   return (
     <div>
