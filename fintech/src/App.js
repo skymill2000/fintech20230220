5a5a9af60a9d4ca7d3cc4271d71552fe8c18f827
유관우 <skymill2000@gmail.com>
2023-02-21 13:49:13 +0900

    [feat] react jsx html 변경  - html 테이블 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -6,10 +6,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>핀테크 아카데미에 오신것을 환영합니다.</p>
-        <p>핀테크 아카데미에 오신것을 환영합니다.</p>
-        <p>핀테크 아카데미에 오신것을 환영합니다.</p>
-        <p>핀테크 아카데미에 오신것을 환영합니다.</p>
+        <h1>핀테크 아카데미에 오신것을 환영합니다.</h1>
+        <p>해당하는 기능을 수행합니다.</p>
+        <table>
+          <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+          </tr>
+        </table>
         <a
           className="App-link"
           href="https://reactjs.org"
