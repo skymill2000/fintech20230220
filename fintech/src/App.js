f21db3eb24a883ca21630024bf8684affd47354d
유관우 <skymill2000@gmail.com>
2023-02-21 15:28:58 +0900

    [feat] ListComponent 추가  - 리스트 출력 실습용 파일 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import InputComponents from "./components/InputComponent";
+import ListComponents from "./components/ListComponents";
 import Welcome from "./components/Welcome";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Welcome userName="유관우" userAge={35} userHeight={175}></Welcome>
       <InputComponents></InputComponents>
+      <ListComponents></ListComponents>
     </div>
   );
 }
---
+++ fintech/src/components/ListComponents.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListComponents = () => {
+  return <div>ListComponents</div>;
+};
+
+export default ListComponents;
