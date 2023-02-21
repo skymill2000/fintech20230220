3f92cfb4ed74a2aa810516f250384acdb6c065a9
유관우 <skymill2000@gmail.com>
2023-02-21 15:32:58 +0900

    [feat] list Array 추가  - map 을 활용하여 Welecome 컴포넌트 렌더링

--- fintech/src/components/ListComponents.js
+++ fintech/src/components/ListComponents.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Welcome from "./Welcome";
 
 const ListComponents = () => {
-  return <div>ListComponents</div>;
+  const [list, setList] = useState([
+    { username: "홍길동", age: 33, height: 183 },
+    { username: "고길동", age: 43, height: 123 },
+    { username: "둘리", age: 23, height: 133 },
+  ]);
+  return (
+    <div>
+      {list.map((user) => {
+        return (
+          <Welcome
+            userName={user.username}
+            userAge={user.age}
+            userHeight={user.height}
+          ></Welcome>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ListComponents;
