4d7761a3f299338848f4086e7f25a95dedaf0fe0
유관우 <skymill2000@gmail.com>
2023-02-21 16:23:29 +0900

    [feat] 라우팅 예제 리스트 라우팅 추가  - 리스트 컴포넌트를 렌더링 하는 라우터 추가

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InputComponents from "./components/InputComponent";
 import Welcome from "./components/Welcome";
+import ListComponents from "./components/ListComponents";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Welcome />}></Route>
         <Route path="/input" element={<InputComponents />}></Route>
+        <Route path="/list" element={<ListComponents />}></Route>
       </Routes>
     </BrowserRouter>
   );
