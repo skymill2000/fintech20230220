93f504def2d3491da9b32827857d2b08500bb2b8
유관우 <skymill2000@gmail.com>
2023-02-21 14:52:30 +0900

    [feat] 이벤트 바인딩  - input 요소에 onChange Event 바인딩

--- fintech/src/components/InputComponent.js
+++ fintech/src/components/InputComponent.js
@@ -3,7 +3,16 @@ import { useState } from "react";
 const InputComponents = (props) => {
   const value = "test";
   const [name, setName] = useState("test");
-  return <p>{value}</p>;
+  const handleChangeInput = (event) => {
+    console.log(event.target.value);
+  };
+  return (
+    <>
+      <p>{value}</p>
+      <input onChange={handleChangeInput}></input>
+      <button>입력</button>
+    </>
+  );
 };
 
 export default InputComponents;
