61afdf8a4976d00ce405dd13de64c3487ed9d1e2
유관우 <skymill2000@gmail.com>
2023-02-21 16:21:15 +0900

    [feat] react-router-dom  - 리엑트 라우터 돔 추가  - npm install react-router-dom

--- fintech/src/App.js
+++ fintech/src/App.js
@@ -1,17 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import InputComponents from "./components/InputComponent";
-import ListComponents from "./components/ListComponents";
-import StyledComponent from "./components/StyledComponent";
 import Welcome from "./components/Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <Welcome userName="유관우" userAge={35} userHeight={175}></Welcome>
-      <InputComponents></InputComponents>
-      <ListComponents></ListComponents>
-      <StyledComponent></StyledComponent>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Welcome />}></Route>
+        <Route path="/input" element={<InputComponents />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- fintech/src/components/StyledComponent.js
+++ fintech/src/components/StyledComponent.js
@@ -2,7 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const Button = styled.button`
-  color: grey;
+  background: #1c6ea4;
+  background: -moz-linear-gradient(left, #1c6ea4 0%, #2388cb 50%, #144e75 100%);
+  background: -webkit-linear-gradient(
+    left,
+    #1c6ea4 0%,
+    #2388cb 50%,
+    #144e75 100%
+  );
+  background: linear-gradient(to right, #1c6ea4 0%, #2388cb 50%, #144e75 100%);
+  -webkit-box-shadow: 3px 9px 6px 5px #000000;
+  box-shadow: 3px 9px 6px 5px #000000;
 `;
 
 const StyledComponent = () => {
