1667179bf6563212e4d01e5e0e99fa099d57d173
유관우 <skymill2000@gmail.com>
2023-02-21 10:09:24 +0900

    [feat] React 프로젝트 추가  - npx create-react-app fintech 명령어를 통해 fintech 리액트 프로젝트 추가

---
+++ fintech/src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>핀테크 아카데미에 오신것을 환영합니다.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
