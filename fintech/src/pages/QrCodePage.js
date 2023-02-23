2b5f4ba122d4518fc957fa4bde2ad70c004638a2
유관우 <skymill2000@gmail.com>
2023-02-23 15:22:55 +0900

    [feat] QR 코드 생성 완료  - npm install qrcode.react  - 핀테크 번호로 QR 코드 생성 완료

--- fintech/src/pages/QrCodePage.js
+++ fintech/src/pages/QrCodePage.js
@@ -1,10 +1,28 @@
 import React from "react";
 import AppHeader from "../components/Common/AppHeader";
+import { QRCodeSVG } from "qrcode.react";
+import styled from "styled-components";
+import { useLocation } from "react-router-dom";
+import queryString from "query-string";
+
+const QRBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 1rem;
+`;
 
 const QrCodePage = () => {
+  const search = useLocation().search;
+  const fintechUseNo = queryString.parse(search).fintechUseNo;
+
   return (
     <div>
       <AppHeader title={"QR 코드 생성"}></AppHeader>
+      <QRBlock>
+        <QRCodeSVG size={200} value={fintechUseNo}></QRCodeSVG>
+        <p>fintech 번호 : {fintechUseNo}</p>
+      </QRBlock>
     </div>
   );
 };
