454124761a9225dfadb5ed6901828059a27dc9ed
유관우 <skymill2000@gmail.com>
2023-02-24 11:03:41 +0900

    [work] work07 출금 이체 기능 구현 2/2 - 출금 이체 기능 완성

--- fintech/src/components/Withdraw/ModalCard.js
+++ fintech/src/components/Withdraw/ModalCard.js
@@ -33,7 +33,8 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
 
   const genTransId = () => {
     let countnum = Math.floor(Math.random() * 1000000000) + 1;
-    let transId = "M202201564U" + countnum; //이용기과번호 본인것 입력
+    const clientNo = "M202300440";
+    let transId = clientNo + "U" + countnum; //이용기관번호 본인것 입력
     return transId;
   };
 
@@ -44,6 +45,27 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
     // axios option으로 요청을 작성하기 <- api 요청
     // application/json 은 데이터를 어떻게 전송?
     // 결과를 로그로 작성
+
+    const accessToken = localStorage.getItem("accessToken");
+
+    const data = {
+      bank_tran_id: genTransId(),
+      cntr_account_type: "N",
+      cntr_account_num: "100000000001",
+      dps_print_content: "이용료（홍길동）",
+      fintech_use_num: fintechUseNo,
+      wd_print_content: "사용료",
+      tran_amt: amount,
+      tran_dtime: "20230223152400",
+      req_client_name: "홍길동",
+      req_client_fintech_use_num: fintechUseNo,
+      req_client_num: "SKYMILL2000",
+      transfer_purpose: "ST",
+      recv_client_name: "유관우",
+      recv_client_bank_code: "097",
+      recv_client_account_num: "100000000001",
+    };
+
     const option = {
       method: "POST",
       url: "/v2.0/transfer/withdraw/fin_num",
@@ -52,6 +74,10 @@ const ModalCard = ({ bankName, fintechUseNo, tofintechno }) => {
       },
       data: data,
     };
+
+    axios(option).then(({ data }) => {
+      console.log(data);
+    });
   };
 
   const deposit = () => {};
