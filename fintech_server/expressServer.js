d7cc3cad5023f5ad7d6fbbb67e362a109ec61f86
유관우 <skymill2000@gmail.com>
2023-02-24 15:21:52 +0900

    [feat] 거래내역 조회 서버 이전  - 거래내역 조회 결과를 서버에서 구현합니다.

--- fintech_server/expressServer.js
+++ fintech_server/expressServer.js
@@ -1,9 +1,60 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
+const accessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAwMDM0NzM2Iiwic2NvcGUiOlsiaW5xdWlyeSIsImxvZ2luIiwidHJhbnNmZXIiXSwiaXNzIjoiaHR0cHM6Ly93d3cub3BlbmJhbmtpbmcub3Iua3IiLCJleHAiOjE2ODQ4OTYyODcsImp0aSI6IjEwNDFiMmE4LTM5ZmMtNDg2NC04YzQwLWM2NWI4ZjBiODRhYyJ9.muvvn9dl_SR-dzY0Il5-v3-RCPZpiVWBuGfU_1EsT6k"; //데이터베이스 에서 가져온 혹은 클라이언트에서 전달받은 accessToken
 
 app.get("/", function (req, res) {
   console.log("사용자의 요청이 발생했습니다.");
   res.send("Hello World");
 });
+
+app.get("/user", (req, res) => {
+  res.json("get method");
+});
+
+app.post("/user", (req, res) => {
+  res.json("post method");
+});
+
+app.delete("/user", (req, res) => {
+  res.json("delete method");
+});
+
+app.put("/user", (req, res) => {
+  res.json("put method");
+});
+
+app.get("/transaction", (req, res) => {
+  const option = {
+    method: "GET",
+    url: "https://testapi.openbanking.or.kr/v2.0/account/transaction_list/fin_num",
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+    params: {
+      bank_tran_id: genTransId(),
+      fintech_use_num: "120230044088951024630415",
+      inquiry_type: "A",
+      inquiry_base: "D",
+      from_date: "20230223",
+      to_date: "20230223",
+      sort_order: "D",
+      tran_dtime: "20230223114800",
+    },
+  };
+  axios(option).then(({ data }) => {
+    console.log(data);
+    //mysql 데이터 입력 쿼리
+    res.json(data);
+  });
+});
+
+const genTransId = () => {
+  let countnum = Math.floor(Math.random() * 1000000000) + 1;
+  const clientNo = "M202300440";
+  let transId = clientNo + "U" + countnum; //이용기관번호 본인것 입력
+  return transId;
+};
 
 app.listen(4000);
